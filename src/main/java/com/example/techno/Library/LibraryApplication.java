2b218ba7c561e169e9de2e00cbe09def9d642251
Abhi Raj <raj12oct2001@gmail.com>
2025-07-17 00:38:18 +0530

    initial commit

---
+++ src/main/java/com/example/techno/Library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.techno.Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
